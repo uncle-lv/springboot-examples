233180236c9a383565ff49b7f7b33d12bce79ecc
uncle-lv <uncle.lv@outlook.com>
2021-10-01 18:15:24 +0800

    🦄 refactor(SecurityConfig.java): PasswordEncoder bean
    Encapsulate PasswordEncoder bean into method passwordEncoder

--- spring-security/src/main/java/org/example/demo/config/SecurityConfig.java
+++ spring-security/src/main/java/org/example/demo/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package org.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -24,10 +26,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-                .withUser("user").password(new BCryptPasswordEncoder().encode("pwd")).roles("USER");
+        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
+                .withUser("user").password(passwordEncoder().encode("pwd")).roles("USER");
 
-        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-                .withUser("admin").password(new BCryptPasswordEncoder().encode("pwd")).roles("USER", "ADMIN");
+        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
+                .withUser("admin").password(passwordEncoder().encode("pwd")).roles("USER", "ADMIN");
+    }
+
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
