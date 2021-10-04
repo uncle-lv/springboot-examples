76dcd95522d079de95fa1484d44249b5b6ad5eed
uncle-lv <uncle.lv@outlook.com>
2021-10-04 17:58:55 +0800

    🐎 ci(spring-security-with-db/sql): update sql file

--- spring-security-with-db/sql/spring_security_with_db.sql
+++ spring-security-with-db/sql/spring_security_with_db.sql
@@ -16,22 +16,57 @@
 CREATE DATABASE IF NOT EXISTS `spring_security_with_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `spring_security_with_db`;
 
+-- 导出  表 spring_security_with_db.role 结构
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- 正在导出表  spring_security_with_db.role 的数据：~2 rows (大约)
+DELETE FROM `role`;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` (`id`, `name`) VALUES
+	(1, 'ADMIN'),
+	(2, 'USER');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+
+-- 导出  表 spring_security_with_db.role_user 结构
+CREATE TABLE IF NOT EXISTS `role_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_role_user_user` (`user_id`),
+  KEY `FK_role_user_role` (`role_id`),
+  CONSTRAINT `FK_role_user_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  CONSTRAINT `FK_role_user_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- 正在导出表  spring_security_with_db.role_user 的数据：~3 rows (大约)
+DELETE FROM `role_user`;
+/*!40000 ALTER TABLE `role_user` DISABLE KEYS */;
+INSERT INTO `role_user` (`id`, `user_id`, `role_id`) VALUES
+	(1, 2, 1),
+	(2, 2, 2),
+	(3, 1, 2);
+/*!40000 ALTER TABLE `role_user` ENABLE KEYS */;
+
 -- 导出  表 spring_security_with_db.user 结构
 CREATE TABLE IF NOT EXISTS `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
   `password` varchar(80) NOT NULL,
-  `roles` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- 正在导出表  spring_security_with_db.user 的数据：~2 rows (大约)
 DELETE FROM `user`;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`id`, `username`, `email`, `password`, `roles`) VALUES
-	(1, 'user', 'user@outlook.com', '$2a$10$Rz6x1Ox2jhUlbfSg5JDABOB7awayLwSdmCF3Fj4dISaB9d7OsgLI2', 'USER'),
-	(2, 'admin', 'admin@outlook.com', '$2a$10$Rz6x1Ox2jhUlbfSg5JDABOB7awayLwSdmCF3Fj4dISaB9d7OsgLI2', 'USER,ADMIN');
+INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES
+	(1, 'user', 'user@outlook.com', '$2a$10$Rz6x1Ox2jhUlbfSg5JDABOB7awayLwSdmCF3Fj4dISaB9d7OsgLI2'),
+	(2, 'admin', 'admin@outlook.com', '$2a$10$Rz6x1Ox2jhUlbfSg5JDABOB7awayLwSdmCF3Fj4dISaB9d7OsgLI2');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
